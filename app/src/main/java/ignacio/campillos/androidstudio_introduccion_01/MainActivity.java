27e262b8fd9e85ace8330042fa89cf59e4890f00
Ignacio Campillos Serrano <ignaciocampillosserrano@iMac-02.local>
2023-09-13 10:40:08 +0200

    Initial commit

---
+++ app/src/main/java/ignacio/campillos/androidstudio_introduccion_01/MainActivity.java
@@ -0,0 +1,14 @@
+package ignacio.campillos.androidstudio_introduccion_01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
